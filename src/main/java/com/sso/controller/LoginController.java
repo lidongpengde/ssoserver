1fd29de7279b19eddcaeb91b95edd5cc541f6698
lidongpeng <821888642@qq.com>
2018-02-26 17:06:55 +0800

    Initial commit

---
+++ src/main/java/com/sso/controller/LoginController.java
@@ -0,0 +1,90 @@
+package com.sso.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+/**
+ * Created by lidongpeng on 2018/2/26.
+ */
+@Controller
+public class LoginController {
+    Map<String,String> cache=new HashMap<String, String>();
+    @RequestMapping("/toLogin")
+    public String toLogin(HttpServletRequest request){
+        String querystr=request.getParameter("redirect");
+        String gloabal=null;
+        Cookie cookie=getCookieByName(request, "globalCookie");
+        if (cookie!=null){
+             gloabal=cookie.getValue();
+             if (!StringUtils.isEmpty(gloabal)){
+                 String redirect= (String) request.getSession().getAttribute("redirect");
+                 StringBuffer buffer=new StringBuffer(redirect);
+                 buffer.append("?");
+                 //生成ticket
+                 String ticket=UUID.randomUUID().toString().substring(0,6);
+                 cache.put(ticket,gloabal);
+                 buffer.append("ticket="+ticket);
+                 return "redirect:"+buffer.toString();
+             }
+        }
+        request.getSession().setAttribute("redirect",querystr);
+        return "login";
+    }
+
+    @RequestMapping("/authPrinciple")
+    public String authPrinciple(HttpServletRequest request, HttpServletResponse response){
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        //验证用户名密码,此处省略
+        //构建回跳地址
+        String redirect= (String) request.getSession().getAttribute("redirect");
+        StringBuffer buffer=new StringBuffer(redirect);
+        buffer.append("?");
+        //生成ticket
+        String ticket=UUID.randomUUID().toString().substring(0,6);
+        Cookie cookie=new Cookie("globalCookie",username);
+        response.addCookie(cookie);
+        cache.put(ticket,username);
+        buffer.append("ticket="+ticket);
+        return "redirect:"+buffer.toString();
+    }
+    @RequestMapping(value = "/tickets/{ticket}", method = RequestMethod.POST)
+    public final ResponseEntity<String> createServiceTicket(@PathVariable("ticket") final String ticket) {
+        try {
+            String result= cache.get(ticket);
+            if (result!=null && result!=""){
+                return new ResponseEntity<String>(result, HttpStatus.OK);
+            }
+        } catch (final Exception e) {
+
+    }
+        return new ResponseEntity<String>("验证token出错", HttpStatus.BAD_REQUEST);
+    }
+    public  Cookie getCookieByName(HttpServletRequest request, String name) {
+         Cookie[] cookieMap = request.getCookies();
+         if (cookieMap==null)
+             return null;
+         for (Cookie cookie:cookieMap){
+             if (cookie.getName().equals(name)){
+                 return cookie;
+             }
+         }
+         return null;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lidongpeng
+  Date: 2018/2/26
+  Time: 11:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="authPrinciple">
+    <input type="text" placeholder="username" name="username"/>
+    <input type="password" placeholder="password" name="password"/>
+    <button>submit</button>
+</form>
+</body>
+</html>
